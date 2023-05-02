44e6263cebd9134ea48fb99d2b4b3868e8d34d32
regvsn <reginald.appiah7@gmail.com>
2023-05-02 17:37:24 -0400

    Initial Flutter Setup

---
+++ android/app/src/main/kotlin/com/example/purematch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.purematch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pure Match',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AdminConsole(title: 'Admin Console'),
+    );
+  }
+}
+
+class AdminConsole extends StatelessWidget {
+  const AdminConsole({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
