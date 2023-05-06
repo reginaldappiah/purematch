25eac8c030a25f1da575a945b0b7e8982001c120
regvsn <reginald.appiah7@gmail.com>
2023-05-05 23:56:18 -0400

    TextEditingController Update

--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -27,35 +27,60 @@ class AdminPage extends StatelessWidget {
         return Column(
           children: [
             Container(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                  left: 15,
-                  right: 15,
-                ),
-                child: CupertinoSearchTextField(
-                    controller: TextEditingController(text: snapshot.searchText),
-                    onChanged: (value) {
-                      //Results in a bloc lookup each text field update.
-                      final adminSearchBarCubit = context.read<AdminSearchBarCubit>();
-                      if (value.isEmpty) {
-                        //Emits Search Bar Empty State
-                      } else {
-                        //Emits Search Bar Filled State
-                      }
-                    })),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(),
-                    title: Text(
-                      dummyData[index],
-                    ),
-                    subtitle: Text("Member since 11/25/2019"),
-                  );
-                },
-                itemCount: dummyData.length,
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CupertinoSearchTextField(
+                        controller: snapshot.controller,
+                        style: const TextStyle(color: Colors.white),
+                        onChanged: (value) {
+                          //Results in a bloc lookup each text field update.
+
+                          final adminSearchBarCubit = context.read<AdminSearchBarCubit>();
+                          if (value.isEmpty) {
+                            adminSearchBarCubit.emitEmptyState();
+                          } else {
+                            adminSearchBarCubit.emitUpdateState(snapshot.controller);
+                          }
+                        }),
+                  ),
+                  snapshot is AdminSearchBarUpdate
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: InkWell(
+                            onTap: () {
+                              //Results in a bloc lookup each text field cancel.
+                              final adminSearchBarCubit = context.read<AdminSearchBarCubit>();
+                              adminSearchBarCubit.emitEmptyState();
+                            },
+                            child: const Text(
+                              "Cancel",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
               ),
+            ),
+            Expanded(
+              child: snapshot is AdminSearchBarEmpty
+                  ? Container()
+                  : ListView.builder(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const CircleAvatar(),
+                          title: RichText(
+                            text: TextSpan(
+                              text: dummyData[index],
+                            ),
+                          ),
+                          subtitle: const Text("Member since 11/25/2019"),
+                        );
+                      },
+                      itemCount: dummyData.length,
+                    ),
             ),
           ],
         );
